9f6dc553794a74939bea2313c7736590224b2693
JupiterSamin <jupiter@ess-tech.pro>
2026-01-18 22:40:53 +0700

    update(other): :jigsaw: update
    update

--- server/server-build.js
+++ server/server-build.js
@@ -4,31 +4,26 @@ const { spawn } = require('child_process')
 const serverDistPath = './server/dist'
 
 if (fs.existsSync(serverDistPath)) {
-	try {
-		fs.emptyDirSync(serverDistPath)
-	} catch (err) {
-		console.error(err)
-	}
-	try {
-		fs.remove(serverDistPath)
-	} catch (err) {
-		console.error(err)
-	}
+  try {
+    fs.emptyDirSync(serverDistPath)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 const child = spawn(
-	'sucrase',
-	['--quiet ./server/src -d ./server/dist --transforms typescript,imports'],
-	{
-		stdio: 'inherit',
-		shell: true,
-	}
+  'sucrase',
+  ['--quiet ./server/src -d ./server/dist --transforms typescript,imports'],
+  {
+    stdio: 'inherit',
+    shell: true,
+  }
 )
 
 child.on('close', (code) => {
-	if (code !== 0) {
-		console.error(`Sucrase exited with code ${code}`)
-		process.exit(code)
-	}
-	console.log('Sucrase build completed successfully')
+  if (code !== 0) {
+    console.error(`Sucrase exited with code ${code}`)
+    process.exit(code)
+  }
+  console.log('Sucrase build completed successfully')
 })
