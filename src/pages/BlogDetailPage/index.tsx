da8e16090eef3890fc612c91e28efb96b820c80c
jupiter <jupiter@ess-tech.pro>
2026-02-16 21:08:43 +0700

    feat(blog-detail): add back navigation button and header styling

--- src/pages/BlogDetailPage/index.tsx
+++ src/pages/BlogDetailPage/index.tsx
@@ -1,12 +1,22 @@
 import { useGetPokemonBlogDetailQuery } from 'app/apis/blog'
+import { useNavigateInfo } from 'app/router/context/InfoContext'
 import Image from 'components/common/Image'
 import { getBlogDetailPath } from 'utils/ApiHelper'
 import BlogDetailLoading from './loading'
-import { BlogDetailPageStyle, TitleStyle } from './styles'
+import {
+  BackButtonStyle,
+  BackIconStyle,
+  BlogDetailPageStyle,
+  HeaderStyle,
+  TitleStyle,
+} from './styles'
 import { generateDescription, generateShortDescription } from './utils'
 
 const BlogDetailPage = () => {
   const route = useRoute()
+  const navigateInfo = useNavigateInfo()
+  const navigate = useNavigate()
+
   const { slug } = route.params
   const { data = getAPIStore(getBlogDetailPath(slug)), isFetching } =
     useGetPokemonBlogDetailQuery(slug)
@@ -45,8 +55,19 @@ const BlogDetailPage = () => {
     }
   }, [JSON.stringify(blogDetailState), shortDescription])
 
+  const handleBack = () => {
+    if (!navigateInfo.from) return navigate(import.meta.env.ROUTER_BLOGS_PATH)
+
+    navigate(-1)
+  } // handleBack
+
   return (
     <BlogDetailPageStyle>
+      <HeaderStyle>
+        <BackButtonStyle onClick={handleBack}>
+          <BackIconStyle />
+        </BackButtonStyle>
+      </HeaderStyle>
       {isShowLoading ? (
         <BlogDetailLoading />
       ) : (
--- src/pages/BlogDetailPage/styles.tsx
+++ src/pages/BlogDetailPage/styles.tsx
@@ -19,6 +19,41 @@ export const BlogDetailPageStyle = styled.div`
   }
 `
 
+export const HeaderStyle = styled.div`
+  position: sticky;
+  height: 24px;
+  margin-top: -16px;
+  top: 0;
+  z-index: 10;
+`
+
+export const BackButtonStyle = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: none;
+  border: none;
+  height: 36px;
+  width: 36px;
+  user-select: none;
+  margin-left: -12px;
+  top: -12px;
+  z-index: 10;
+  ${import.meta.env.STYLE_MIXINS_LIQUID_GLASS}
+  border-radius: 14px;
+  background: rgba(121, 120, 120, 0.75);
+`
+
+export const BackIconStyle = styled.span`
+  border: solid #ffffff;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+`
+
 export const ImageLoadingStyle = styled.div`
   width: calc(100% - 16px);
   padding-bottom: calc(100% * 13 / 16);
